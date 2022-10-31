253f2ee2f4043c33b352a34f52e8069df10fd99a
Kęstutis Miknevičius <miknevicius.kestutis@gmail.com>
2022-10-31 19:01:02 +0000

    fix: enable/disable pwm on lafrite player

--- LaFriteMelodyPlayer.py
+++ LaFriteMelodyPlayer.py
@@ -24,8 +24,10 @@ class LaFriteMelodyPlayerImpl:
         duration = duration / 1000
         self.pwm.set_period(freq, True)
         self.pwm.set_duty_cycle_percent(50)
+        self.pwm.enable()
         sleep(duration)
         self.pwm.set_duty_cycle(0)
+        self.pwm.disable()
 
 # TODO remove!
 
@@ -33,20 +35,52 @@ from pynoteplayer.Melody import Melody
 from pynoteplayer.Note import Note
 
 
-class SamsungNotification(Melody):
+class HarryPoterHedwigsTheme(Melody):
     def __init__(self, player=None) -> None:
-        self.tempo_bpm = 290
+        self.tempo_bpm = 144
         self.time_signature = {
-            'beats': 4,
+            'beats': 3,
             'beat-type': 4
         }
         super().__init__(self.tempo_bpm, self.time_signature, player)
 
         self.notes = [
-            Note('B4', 4), Note('Ds5', 4), Note('B5', 4),
-            Note('As5', 8), Note('REST', 8), Note('REST', 8),
-            Note('REST', 4), Note('Fs5', 8), Note('REST ', 8),
+            Note('REST', 2), Note('D4', 4),
+            Note('G4', 4, 1), Note('As4', 8), Note('A4', 4),
+            Note('G4', 2), Note('D5', 4),
+            Note('C5', 2, 1),
+            Note('A4', 2, 1),
+            Note('G4', 4, 1), Note('As4', 8), Note('A4', 4),
+            Note('F4', 2), Note('Gs4', 4),
+            Note('D4', 1, 1),
+            Note('D4', 4),
+
+            Note('G4', 4, 1), Note('As4', 8), Note('A4', 4),
+            Note('G4', 2), Note('D5', 4),
+            Note('F5', 2), Note('E5', 4),
+            Note('Ds5', 2), Note('B4', 4),
+            Note('Ds5', 4, 1), Note('D5', 8), Note('Cs5', 4),
+            Note('Cs4', 2), Note('B4', 4),
+            Note('G4', 1, 1),
+            Note('As4', 4),
+
+            Note('D5', 2), Note('As4', 4),
+            Note('D5', 2), Note('As4', 4),
+            Note('Ds5', 2), Note('D5', 4),
+            Note('Cs5', 2), Note('A4', 4),
+            Note('As4', 4, 1), Note('D5', 8), Note('Cs5', 4),
+            Note('Cs4', 2), Note('D4', 4),
+            Note('D5', 1, 1),
+            Note('REST', 4), Note('As4', 4),
+
+            Note('D5', 2), Note('As4', 4),
+            Note('D5', 2), Note('As4', 4),
+            Note('F5', 2), Note('E5', 4),
+            Note('Ds5', 2), Note('B4', 4),
+            Note('Ds5', 4, 1), Note('D5', 8), Note('Cs5', 4),
+            Note('Cs4', 2), Note('As4', 4),
+            Note('G4', 1, 1)
         ]
 
 
-SamsungNotification(LaFriteCapableMelodyPlayer()).play()
+HarryPoterHedwigsTheme(LaFriteCapableMelodyPlayer()).play()
